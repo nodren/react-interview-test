48ec162082151fc78fe3f016e4aaabcdd8f3cd53
Ben Rogers <nodren@gmail.com>
2022-10-11 20:54:29 +0000

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,14 @@
-import React, { FC } from 'react'
-import { Container, Navbar } from 'react-bootstrap'
+import React, { FC } from "react";
+import { Container, Navbar } from "react-bootstrap";
 
 const Header: FC = () => {
 	return (
 		<Navbar bg="dark" variant="dark">
 			<Container>
-				<Navbar.Brand>
-					eVRM Intervew Project
-				</Navbar.Brand>
+				<Navbar.Brand>eVRM Interview Project</Navbar.Brand>
 			</Container>
 		</Navbar>
-	)
-}
+	);
+};
 
-export default Header
+export default Header;
